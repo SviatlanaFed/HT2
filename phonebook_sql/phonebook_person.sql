57fb2e671daf47667bc73900be5370822e2f3704
sviatlanafed <sveta.bychkova@gmail.com>
2018-04-02 11:16:40 +0200

    second commit

---
+++ phonebook_sql/phonebook_person.sql
@@ -0,0 +1,5 @@
+UPDATE phonebook.person SET surname = 'Иванов', name = 'Иван', middlename = 'Иванович' WHERE id = 1;
+UPDATE phonebook.person SET surname = 'Петров', name = 'Пётр', middlename = 'Петрович' WHERE id = 2;
+UPDATE phonebook.person SET surname = 'Сидоров', name = 'Сидор', middlename = 'Сидорович' WHERE id = 3;
+UPDATE phonebook.person SET surname = 'Смит', name = 'Джон', middlename = '' WHERE id = 4;
+UPDATE phonebook.person SET surname = 'Федоренчик', name = 'Светлана', middlename = 'Андреевна' WHERE id = 17;
---
+++ phonebook_sql/phonebook_phone.sql
@@ -0,0 +1,7 @@
+UPDATE phonebook.phone SET owner = 1, number = '+37529-1112201' WHERE id = 1;
+UPDATE phonebook.phone SET owner = 1, number = '+37533-4445566' WHERE id = 2;
+UPDATE phonebook.phone SET owner = 2, number = '+37544-0009999' WHERE id = 3;
+UPDATE phonebook.phone SET owner = 2, number = '+37517-2990011' WHERE id = 4;
+UPDATE phonebook.phone SET owner = 3, number = '+37517-1117733' WHERE id = 5;
+UPDATE phonebook.phone SET owner = 4, number = '+37544-1119922' WHERE id = 6;
+UPDATE phonebook.phone SET owner = 17, number = '+37525-7557605' WHERE id = 33;
